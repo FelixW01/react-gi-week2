a6d054b616d4657d2dadb37d3785661ec90699ae
Felix W <felixwillem01@yahoo.com>
2025-01-21 20:52:13 -0500

    worked on movie-search logic

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 import './App.css'
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [searchInput, setSearchInput] = useState("")
+
+ const handleSubmit = async () => {
+    console.log(searchInput, '<< searchInput')
+    // This is vite's way of doing .ENV
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&query=${encodeURIComponent(searchInput)}&include_adult=false&language=en-US&page=1`;
+    try {
+    const response = await axios.get(url);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+   }
+ }  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Movie-DB</h1>
+    <input id="searchInput" type="text" placeholder="Search a movie!" onChange={(e) => setSearchInput(e.target.value)} required></input>
+    <button id="searchBtn" onClick={handleSubmit}>Search</button>
     </>
   )
 }
