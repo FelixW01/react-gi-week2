3c01f2a2c4f31b74f710568d5db9ed4582c61b9a
Felix W <felixwillem01@yahoo.com>
2025-01-22 21:41:45 -0500

    finished create and delete card features

---
+++ todo-list-app/src/components/card/card.jsx
@@ -0,0 +1,43 @@
+import './Card.css'
+import { useNavigate } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+function Card({ tasks, deleteTask }) {
+  const navigate = useNavigate();
+
+  // The delete button was having event bubbling issues making it deleting every single card instead of the one specified with the index
+  // This function acts as a middleware that triggers e.stopPropagation to stop bubbling then run deleteTask
+  const handleDelete = (e, index) => {
+    e.stopPropagation();
+    deleteTask(index);
+  };
+
+  return (
+    <>
+      {tasks && tasks.length > 0 ? (
+        <div className="card-container">
+          {tasks.map((task, index) => {
+            return (
+              <div className="card" onClick={() => navigate('/taskdetail')} key={index}>
+                <div className="card-body">
+                  <p className="card-title">{task.title}</p>
+                  <p className="card-text">{task.description}</p>
+                  <button onClick={(e) => handleDelete(e, index)}>Delete</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+// The application still works without this, but strictmode wants you to declare the propTypes
+// This reminds me of TypeScript
+Card.propTypes = {
+  tasks: PropTypes.array.isRequired, 
+  deleteTask: PropTypes.func.isRequired,
+};
+
+export default Card
--- todo-list-app/src/pages/homepage.jsx
+++ todo-list-app/src/pages/homepage.jsx
@@ -1,11 +1,78 @@
-import '../App.css'
+import './homepage.css';
+import { useState, useEffect } from 'react';
+import Card from '../components/card/card'
 
-function homePage() {
+// Develop a to-do list app with React hooks and React Route
+// Implement features like adding, editing, and deleting tasks
+// Use React Router to navigate between different pages, such as a task list and a task detail page.
+
+function HomePage() {
+  const [tasks, setTasks] = useState([])
+  const [userInput, setUserInput] = useState('')
+  const [userDescription, setUserDescription] = useState('')
+
+  const handleSubmit = () => {
+    if(userInput.trim() !== '') {
+      // Tracks tasks into state and also re track localStorage with the newest iteration
+      setTasks((prevTasks) => {
+        const updatedTasks = [...prevTasks, {title: userInput, description: userDescription}];
+
+        localStorage.setItem('tasks', JSON.stringify(updatedTasks))
+
+        return updatedTasks;
+      });
+      
+      // Resets input state after submission
+      setUserInput('')
+      setUserDescription('')
+    }
+ }  
+
+ // Function for deleting task, taking in the index which is the key of the card
+ const deleteTask = (index) => {
+
+    setTasks((prevTasks) => {
+      // Filter the prevTasks which is tapping into the previous state of the tasks, takes out the task based on the index passed from the card component
+      const updatedTasks = prevTasks.filter((task, taskIndex) => taskIndex !== index);
+
+      // Resets the localstorage with the current updated Task
+       localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+
+       return updatedTasks;
+    });
+ };
+
+  // Sets tasks on page mount from localstorage
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks))
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log(tasks, '<<< tasks');
+  }, [tasks]);
+
+  // Lets the user press enter instead on top of being able to click the submit button
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
   return (
     <>
-    <h1>Homepage</h1>
+    <h1>Task Manager</h1>
+    <div className="inputDiv">
+      <input id="searchInput" type="text" placeholder="Start a task!" value={userInput} onChange={(e) => setUserInput(e.target.value)} onKeyDown={handleKeyPress} required></input>
+      <input id="searchInput" type="text" placeholder="Task description" value={userDescription} onChange={(e) => setUserDescription(e.target.value)} onKeyDown={handleKeyPress} required></input>
+      <button id="searchBtn" onClick={handleSubmit}>Start</button>
+    </div>
+    {/* Send tasks and deleteTask as props */}
+    <Card  tasks={tasks} deleteTask={deleteTask} />
     </>
   )
 }
 
-export default homePage
+export default HomePage
